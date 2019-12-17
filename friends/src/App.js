2e5e183716c4654d1b0aa673187bdc3124259f2b
Darragh Ferry <darraghferry@hotmail.com>
2019-12-17 20:35:54 +0000

    able to add friends with user input from form

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./components/Login";
 import { Route, withRouter, Redirect } from "react-router-dom";
 import Friends from "./components/Friends";
+import AddFriend from "./components/AddFriend";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={Login} />
         {/* <Route path="/friends" component={Friends} /> */}
         <PrivateRoute path="/friends" component={Friends} />
+        <PrivateRoute path="/add-friend" component={AddFriend} />
       </header>
     </div>
   );
